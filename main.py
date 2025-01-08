50ec0b4b0de4787ad2b695788c0d304281e3cdbb
Erki <erki@krakul.eu>
2025-01-08 22:03:42 +0200

    Big commit

--- main.py
+++ main.py
@@ -2,9 +2,16 @@ import cv2
 from PIL import ImageGrab, Image
 import time
 import pytesseract
+import logging
+from discord import SyncWebhook
+from settings import DISCORD_WEBHOOK, TESSERACT_PATH, MIN_SIMILIAIRTY, SLEEP_TIME_SECONDS
 
-pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
-END_TURN_SAMPLE_IMAGE = "EndTurnSample.png"
+logging.basicConfig(format='%(asctime)s %(message)s', level=logging.DEBUG)
+
+pytesseract.pytesseract.tesseract_cmd = TESSERACT_PATH
+END_TURN_SAMPLE_IMAGE = "EndTurnSampleHD.png"
+SCREENSHOT_FILE = "Screenshot.png"
+SCREESHOT_CROPPED_FILE = "Screenshot_crop.png"
 
 
 def get_image_similiarity(sample_picture_file, new_picture_file):
@@ -19,25 +26,61 @@ def get_image_similiarity(sample_picture_file, new_picture_file):
     cv2.normalize(hist_img2, hist_img2, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX)
     # Find the metric value
     metric_val = round(cv2.compareHist(hist_img1, hist_img2, cv2.HISTCMP_CORREL), 2)
-    print(f"Similarity Score for image {new_picture_file}: ", metric_val)
+    logging.info(f"Similarity Score for image {new_picture_file}: {metric_val}")
     return metric_val
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    detected = False
+    last_sent_user = None
+    last_detected_text = None
     while True:
-        im = ImageGrab.grab()
-        im.save("Screenshot.png")
-        similiarity = get_image_similiarity(END_TURN_SAMPLE_IMAGE, "Screenshot.png")
-        if similiarity == 1.0:
-            print("END TURN detected")
-            image = cv2.imread("Screenshot.png")
-            gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-            gray = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
-            filename = "Screenshot.png"
-            cv2.imwrite(filename, gray)
+        try:
+            im = ImageGrab.grab()
+        except OSError as e:
+            logging.error(e)
+            time.sleep(SLEEP_TIME_SECONDS)
+            continue
+        im.save(SCREENSHOT_FILE)
+        similiarity = get_image_similiarity(END_TURN_SAMPLE_IMAGE, SCREENSHOT_FILE)
+        if similiarity >= MIN_SIMILIAIRTY and detected is False:
+            detected = True
+            logging.info("END TURN detected")
+            im = Image.open(SCREENSHOT_FILE)
+            width, height = im.size
+       
+            left = width / 3 + 140
+            top = height / 3 + 30
+            right = (2 * width / 3) - 140
+            bottom = (2 * height / 3) - 250
+            im = im.crop((left, top, right, bottom))
+            im.save(SCREESHOT_CROPPED_FILE)
 
-            text = pytesseract.image_to_string(Image.open("Screenshot.png"), lang='eng')
-            print("Read out text", text)
-            break
-        time.sleep(1)
+            text = pytesseract.image_to_string(Image.open(SCREESHOT_CROPPED_FILE), lang='eng')
+            logging.info(f"Read out text from image: {text}")
+
+            if last_detected_text == text:
+                logging.info("Text not changed")
+            else:
+                last_detected_text = text
+                if "attack" in text:
+                    # in case someone is attacked outside of normal turn order
+                    logging.info(text)                    
+                    webhook = SyncWebhook.from_url(DISCORD_WEBHOOK)
+                    webhook.send(text)
+                    logging.info("Sent message to Discord")
+                elif "turn" in text:
+                    player_name = text.split("'s turn")[0]
+                    logging.info(text)
+                    if last_sent_user != player_name:
+                        last_sent_user = player_name
+                        webhook = SyncWebhook.from_url(DISCORD_WEBHOOK)
+                        webhook.send(text)
+                        logging.info("Sent message to Discord")
+                    else:
+                        logging.debug(f"Already sent for player {player_name}")
+        else:
+            logging.debug(f"Similiarity too low: {similiarity}")
+            detected = False
+        time.sleep(SLEEP_TIME_SECONDS)
---
+++ settings_example.py
@@ -0,0 +1,4 @@
+TESSERACT_PATH = r'C:/Program Files/Tesseract-OCR/tesseract.exe'
+DISCORD_WEBHOOK = "https://discordapp.com/api/webhooks/YOUR_WEBHOOK_HERE"
+MIN_SIMILIAIRTY = 0.85
+SLEEP_TIME_SECONDS = 3
